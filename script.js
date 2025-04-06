482223f9dc7ddc38078bf121d6c6e4d07646a985
Nathan <hello@gougoule.ch>
2025-04-06 19:10:38 +0200

    Refactor conversation components to initialize dynamic variables on window load and improve sidebar responsiveness

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ class dynamicVariable {
     }
 
     set(value) {
+        if (this.value === value) {
+            return;
+        }
         this.value = value;
 
         this.updateDisplay();
@@ -19,9 +22,29 @@ class dynamicVariable {
     updateDisplay() {
         this.resetIfs();
 
-        this.updateLoops();
-        this.updateIfs();
-        this.updateJS();
+        var elements = document.querySelectorAll(`[update~="${this.name}"]`);
+
+        let updated = 0;
+        while (updated < elements.length) {
+            const element = elements[updated];
+            updated++;
+
+            if (checkIfParentHidden(element)) {
+                if (element.tagName === "IF-ELEMENT") {
+                    this.updateIf(element, null);
+                } else if (element.tagName === "EACH-ELEMENT") {
+                    this.updateLoop(element);
+                    elements = document.querySelectorAll(
+                        `[update~="${this.name}"]`
+                    );
+                } else if (element.tagName === "JS-ELEMENT") {
+                    this.updateJS(element);
+                }
+            }
+        }
+
+        this.resetLoopsVariables();
+        this.updateDynamicAttributes();
     }
 
     resetIfs() {
@@ -33,7 +56,7 @@ class dynamicVariable {
         });
     }
 
-    updateIfs(from) {
+    updateIf(element, from) {
         if (from) {
             const nextElement = from.nextElementSibling;
             if (nextElement) {
@@ -44,119 +67,122 @@ class dynamicVariable {
                         nextElement.hidden = false;
                     } else {
                         nextElement.hidden = true;
-                        this.updateIfs(nextElement);
+                        this.updateIf(null, nextElement);
                     }
                 } else if (type === "ELSE-ELEMENT") {
                     nextElement.hidden = false;
                 }
             }
             return;
-        }
-
-        const elements = document.querySelectorAll(
-            `if-element[update~="${this.name}"]`
-        );
-        Array.from(elements).forEach((element) => {
+        } else if (element) {
             try {
                 const condition = element.attributes.js.value;
                 if (eval(condition)) {
                     element.hidden = false;
                 } else {
                     element.hidden = true;
-                    this.updateIfs(element);
+                    this.updateIf(null, element);
                 }
             } catch (error) {}
-        });
+        }
     }
 
-    updateJS() {
-        const elements = Array.from(
-            document.querySelectorAll(`js-element[update~="${this.name}"]`)
-        ).filter((element) => {
-            let parent = element.parentElement;
-            while (parent) {
-                if (parent.hidden) {
-                    return false; // Exclude elements with hidden parents
-                }
-                parent = parent.parentElement;
+    updateJS(element) {
+        if ((element.attributes.html ?? { value: "false" }).value == "true") {
+            element.innerHTML = eval(element.attributes.js.value);
+        } else element.textContent = eval(element.attributes.js.value);
+    }
+
+    updateLoop(element) {
+        try {
+            let uuid = uuidV4();
+            element.setAttribute("uuid", uuid);
+
+            while (element.children.length > 1) {
+                element.removeChild(element.lastChild);
             }
-            return true; // Include elements with no hidden parents
-        });
-        Array.from(elements).forEach((element) => {
-            if (
-                (element.attributes.html ?? { value: "false" }).value == "true"
-            ) {
-                element.innerHTML = eval(element.attributes.js.value);
-            } else element.textContent = eval(element.attributes.js.value);
-        });
-    }
 
-    updateLoops() {
-        let elements = document.querySelectorAll(
-            `each-element[update~="${this.name}"]`
-        );
-        let updated = 0;
-        while (updated < elements.length) {
-            const element = elements[updated];
+            const loopList = eval(element.attributes.js.value);
+            loopsVariables[uuid] = loopList;
 
-            try {
-                let uuid = uuidV4();
-                element.setAttribute("uuid", uuid);
+            const loopElement = element.firstElementChild;
+            if (loopElement) {
+                for (let i = 0; i < loopList.length; i++) {
+                    const clonedElement = loopElement.cloneNode(true);
+                    clonedElement.hidden = false;
+                    element.appendChild(clonedElement);
 
-                while (element.children.length > 1) {
-                    element.removeChild(element.lastChild);
-                }
-
-                const loopList = eval(element.attributes.js.value);
-                loopsVariables[uuid] = loopList;
-
-                const loopElement = element.firstElementChild;
-                if (loopElement) {
-                    for (let i = 0; i < loopList.length; i++) {
-                        const clonedElement = loopElement.cloneNode(true);
-                        clonedElement.hidden = false;
-                        element.appendChild(clonedElement);
-
-                        const lastElement = element.lastElementChild;
-                        const allDescendants =
-                            lastElement.querySelectorAll("*");
-                        for (let k = 0; k < allDescendants.length; k++) {
-                            const descendant = allDescendants[k];
-                            if (descendant.attributes.js) {
-                                const jsValue = descendant.attributes.js.value;
-                                descendant.setAttribute(
-                                    "js",
-                                    `(function() { const ${element.attributes.element.value} = loopsVariables['${uuid}'][${i}]; return ${jsValue}})()`
-                                );
-                            }
-                            if (descendant.attributes.onclick) {
-                                const clickValue =
-                                    descendant.attributes.onclick.value;
-                                descendant.setAttribute(
-                                    "onclick",
-                                    `(function() { const ${element.attributes.element.value} = loopsVariables['${uuid}'][${i}]; ${clickValue}})()`
-                                );
+                    const lastElement = element.lastElementChild;
+                    const allDescendants = lastElement.querySelectorAll("*");
+                    for (let k = 0; k < allDescendants.length; k++) {
+                        const descendant = allDescendants[k];
+                        if (Object.keys(descendant.dataset).length) {
+                            for (const key in descendant.dataset) {
+                                if (key.startsWith("dynamic")) {
+                                    descendant.dataset[
+                                        key
+                                    ] = `(function() { const ${element.attributes.element.value} = loopsVariables['${uuid}'][${i}]; return ${descendant.dataset[key]}})()`;
+                                }
                             }
                         }
+
+                        if (descendant.attributes.js) {
+                            const jsValue = descendant.attributes.js.value;
+                            descendant.setAttribute(
+                                "js",
+                                `(function() { const ${element.attributes.element.value} = loopsVariables['${uuid}'][${i}]; return ${jsValue}})()`
+                            );
+                        }
                     }
-                } else {
-                    console.warn(
-                        "No child element found to clone in:",
-                        element
-                    );
                 }
-            } catch (error) {}
-            elements = document.querySelectorAll(
-                `each-element[update~="${this.name}"]`
-            );
-            updated++;
-        }
+            } else {
+                console.warn("No child element found to clone in:", element);
+            }
+        } catch (error) {}
+    }
+
+    updateDynamicAttributes() {
+        const toUpdate = Array.from(
+            document.querySelectorAll(`[data-update~="${this.name}"]`)
+        ).filter((element) => checkIfParentHidden(element));
+
+        toUpdate.forEach((element) => {
+            if (Object.keys(element.dataset).length) {
+                for (const key in element.dataset) {
+                    if (key.startsWith("dynamicJs")) {
+                        const attribute = key
+                            .split("dynamicJs")[1]
+                            .toLowerCase();
+                        const attributeValue = element.dataset[key];
+                        element.setAttribute(attribute, attributeValue);
+                    } else if (key.startsWith("dynamic")) {
+                        const attribute = key.split("dynamic")[1].toLowerCase();
+                        const attributeValue = eval(element.dataset[key]);
+                        element.setAttribute(attribute, attributeValue);
+                    }
+                }
+            }
+        });
+    }
+
+    resetLoopsVariables() {
         Object.keys(loopsVariables).forEach((uuid) => {
             if (!document.querySelector(`[uuid="${uuid}"]`)) {
                 delete loopsVariables[uuid];
             }
         });
     }
+}
+
+function checkIfParentHidden(element) {
+    let parent = element.parentElement;
+    while (parent) {
+        if (parent.hidden) {
+            return false; // Exclude elements with hidden parents
+        }
+        parent = parent.parentElement;
+    }
+    return true; // Include elements with no hidden parents
 }
 
 function uuidV4() {
@@ -230,21 +256,19 @@ class eachElement extends HTMLElement {
 
 customElements.define("each-element", eachElement);
 
-window.onload = function () {
-    const typeElements = document.querySelectorAll(
-        "if-element, elif-element, else-element"
-    );
-    typeElements.forEach((element) => {
-        element.hidden = true;
-    });
+const typeElements = document.querySelectorAll(
+    "if-element, elif-element, else-element"
+);
+typeElements.forEach((element) => {
+    element.hidden = true;
+});
 
-    const eachElements = document.querySelectorAll("each-element");
-    eachElements.forEach((element) => {
-        let span = document.createElement("span");
-        while (element.firstChild) {
-            span.appendChild(element.firstChild);
-        }
-        span.hidden = true;
-        element.appendChild(span);
-    });
-};
+const eachElements = document.querySelectorAll("each-element");
+eachElements.forEach((element) => {
+    let span = document.createElement("span");
+    while (element.firstChild) {
+        span.appendChild(element.firstChild);
+    }
+    span.hidden = true;
+    element.appendChild(span);
+});
