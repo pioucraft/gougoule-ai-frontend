5e3cf8d68b65321a863ef2a03cc8c7ec7c871232
Nathan <hello@gougoule.ch>
2025-05-03 21:19:46 +0200

    Make it better, basically

--- cli/main.go
+++ cli/main.go
@@ -12,22 +12,48 @@ import (
 func main() {
 	token := os.Getenv("GAI_TOKEN")
 	model := os.Getenv("GAI_MODEL")
+	url := os.Getenv("GAI_URL") 
+	fmt.Println(url)
 
+	asciiArt := ` __________________________
+< Welcome to Gougoule AI ! >
+ --------------------------
+   \
+    \
+        .--.
+       |o_o |
+       |:_/ |
+      //   \ \
+     (|     | )
+    /'\_   _/ \
+    \___)=(___/`
+
+	fmt.Println(asciiArt)
+	ask(token, model, url, "")
+}
+
+func ask(token string, model string, url string, conversation string) {
+	fmt.Println("--------------------------")
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter your question: ")
 	question, _ := reader.ReadString('\n')
+	fmt.Println("--------------------------")
 
-	ask(question, token, model)
-}
+	body := []byte{}
+	if conversation == "" {
 
-func ask(question string, token string, model string) {
-	url := "https://gougoule.ch/api/v1/ask"
-	
-	body, _ := json.Marshal(map[string]string{
-		"question": question,
-		"model": model, 
-	})
+		body, _ = json.Marshal(map[string]string{
+			"question": question,
+			"model": model,
+		})
+	} else {
 
+		body, _ = json.Marshal(map[string]string{
+			"question": question,
+			"model": model,
+			"conversation_id": conversation,
+		})
+	}
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(body))
 
 	req.Header.Set("Content-Type", "application/json")
@@ -37,11 +63,16 @@ func ask(question string, token string, model string) {
 	client := &http.Client{}
 	resp, _ := client.Do(req)
 
+	// Get header conversation_id
+	conversation_id := resp.Header.Get("conversation_id")
+
 	//Print the response (plain text stream)
 	scanner := bufio.NewScanner(resp.Body)
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 	}
-	
+
+	ask(token, model, url, conversation_id)
+
 }
 
